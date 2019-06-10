b0263c538c086b99188b49d49bfad631804f8dc4
wate123 <junlin0604@gmail.com>
2019-06-10 11:09:23 -0400

    correct some mistakes

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from utils import NewsContent
 from gensim.models import KeyedVectors
 from gensim.models import Word2Vec
 
-
 # sentences iterable
 sentences = NewsContent(
     './fakenewsnet_dataset', 'politifact', 'fake')
-
-model = Word2Vec(sentences, sg=1, size=100, workers=4)
-
+model = Word2Vec(sentences, sg=1, size=100, workers=4, min_count=1)
+word_vector = model.wv
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from os import walk
 from os.path import join
-
+import json
 # Memory saving loading words
 
 
@@ -9,24 +9,26 @@ class NewsContent(object):
         self.dirname = dirname
         self.site = site
         self.news_type = news_type
+        self.list_news_files = self.get_list_news_files('json')
 
     def __iter__(self):
-        for file_path in self.get_list_news_files('json'):
-            for line in open(file_path):
-                yield line.split()
+        for file_path in self.list_news_files:
+            with open(file_path, 'r') as f:
+                text = json.load(f)['text']
+                # print(text.split())
+                yield text.split()
 
     '''
     Return all the files you want in the provided directory
     @:param directory root direction you want to search
     '''
-
     def get_list_news_files(self, file_ext):
         list_news_files = []
         site_folder = join(self.dirname, self.site)
         news_path = join(site_folder, self.news_type)
         for root, dirs, files in walk(news_path):
             for f in files:
-                if f.endswith("."+file_ext):
+                if f.endswith("."+file_ext) and not dirs:
                     list_news_files.append(join(root, f))
         return list_news_files
 
