9c8c1488b7efa3274b62766579a4af30caded483
wate123 <junlin0604@gmail.com>
2019-06-10 16:15:20 -0400

    preprocessing using gensim

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from utils import NewsContent
 from gensim.models import KeyedVectors
 from gensim.models import Word2Vec
+import re
 
 # sentences iterable
 sentences = NewsContent(
     './fakenewsnet_dataset', 'politifact', 'fake')
+# model = KeyedVectors.load_word2vec_format(fname='./GoogleNews-vectors-negative300.bin', binary=True)
+# words = model.index2word
 model = Word2Vec(sentences, sg=1, size=100, workers=4, min_count=1)
-word_vector = model.wv
+# word_vector = model.wv
 print(len(model.wv.vocab))
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from os import walk
 from os.path import join
-import json, string
+import json, string, re
+from gensim.parsing.preprocessing import remove_stopwords
+from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords, stem_text
 # Memory saving loading words
 
 
@@ -15,22 +17,23 @@ class NewsContent(object):
     def __iter__(self):
         for file_path in self.list_news_files:
             with open(file_path, 'r') as f:
+                CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords]
                 text = json.load(f)['text']
-                table = str.maketrans("","", string.punctuation)
-                rtext = text.translate(table)
-                yield rtext.split()
+                words = preprocess_string(text, filters=CUSTOM_FILTERS)
+                print(words)
+                yield words
 
     '''
     Return files path iterator you want in the provided directory
     @:param directory root direction you want to search
     '''
+
     def get_list_news_files(self, file_ext):
         list_news_files = []
         site_folder = join(self.dirname, self.site)
         news_path = join(site_folder, self.news_type)
         for root, dirs, files in walk(news_path):
             for f in files:
-                if f.endswith("."+file_ext) and not dirs:
+                if f.endswith("." + file_ext) and not dirs:
                     list_news_files.append(join(root, f))
         return iter(list_news_files)
-
