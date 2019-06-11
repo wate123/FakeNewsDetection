bd1c336dbb551a414529349fe01f6160caa2585c
GETT521 <44852625+GETT521@users.noreply.github.com>
2019-06-11 16:05:11 -0400

    removing emojis

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from os import walk
 from os.path import join
-import json, string, re
+import json, re
 from gensim.parsing.preprocessing import remove_stopwords
 from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords, stem_text
 # Memory saving loading words
@@ -14,12 +14,41 @@ class NewsContent(object):
         self.list_news_files = self.get_list_news_files('json')
         # print(self.list_news_files)
 
+        
+    def remove_emoji(text):
+        #using regex to identify all emojis
+        emoji_pattern = re.compile("["
+                        u"\U0001F600-\U0001F64F"  # emoticons
+                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                        u"\U00002702-\U000027B0"
+                        u"\U000024C2-\U0001F251"
+                        u"\U0001f926-\U0001f937"
+                        u'\U00010000-\U0010ffff'
+                        u"\u200d"
+                        u"\u2640-\u2642"
+                        u"\u2600-\u2B55"
+                        u"\u23cf"
+                        u"\u23e9"
+                        u"\u231a"
+                        u"\u3030"
+                        u"\ufe0f"
+            "]+", flags=re.UNICODE)
+    
+        #removing any identified emojis from text 
+        words = emoji_pattern.sub(r'', text)
+        return words
+    
     def __iter__(self):
         for file_path in self.list_news_files:
             with open(file_path, 'r') as f:
                 CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords]
                 text = json.load(f)['text']
-                words = preprocess_string(text, filters=CUSTOM_FILTERS)
+                
+                #removes emojis from text 
+                words = NewsContent.remove_emoji(text)
+                words = preprocess_string(words, filters=CUSTOM_FILTERS)
                 # print(words)
                 yield words
 
