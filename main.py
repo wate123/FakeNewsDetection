aec197691084ddaf968832c7ebdc2d789b4685bb
wate123 <junlin0604@gmail.com>
2019-06-11 17:08:52 -0400

    word plot and get feature

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from utils import NewsContent
 from gensim.models import KeyedVectors
 from gensim.models import Word2Vec
+import numpy as np
+import multiprocessing
+from utils import tsne_similar_word_plot
+
 import re
 
+
+
 # sentences iterable
-sentences = NewsContent(
-    './fakenewsnet_dataset', 'politifact', 'fake')
+words = NewsContent(
+    '../FakeNewsNet/code/fakenewsnet_dataset', 'politifact', 'fake', 'title')
 # model = KeyedVectors.load_word2vec_format(fname='./GoogleNews-vectors-negative300.bin', binary=True)
 # words = model.index2word
-model = Word2Vec(sentences, sg=1, size=100, workers=4, min_count=1)
-# word_vector = model.wv
-print(len(model.wv.vocab))
+model = Word2Vec(words, sg=1, size=100, workers=multiprocessing.cpu_count() *10, min_count=1)
+word_vector = model.wv
+print(len(word_vector.vocab))
+
+tsne_similar_word_plot(model, "trump")
--- utils.py
+++ utils.py
@@ -1,55 +1,63 @@
+import json
+import re
 from os import walk
 from os.path import join
-import json, re
-from gensim.parsing.preprocessing import remove_stopwords
-from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords, stem_text
+
+import matplotlib.pyplot as plt
+from gensim.parsing.preprocessing import preprocess_string, strip_tags, strip_punctuation, strip_multiple_whitespaces, \
+    strip_numeric, remove_stopwords
+# % matplotlib inline
+from sklearn.manifold import TSNE
+
+
 # Memory saving loading words
 
 
 class NewsContent(object):
-    def __init__(self, dirname, site, news_type):
+    def __init__(self, dirname, site, news_type, feature_type):
         self.dirname = dirname
         self.site = site
         self.news_type = news_type
+        self.feature_type = feature_type
         self.list_news_files = self.get_list_news_files('json')
-        # print(self.list_news_files)
 
-        
-    def remove_emoji(text):
-        #using regex to identify all emojis
+    def remove_emoji(self, text):
+        # using regex to identify all emojis
         emoji_pattern = re.compile("["
-                        u"\U0001F600-\U0001F64F"  # emoticons
-                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                        u"\U00002702-\U000027B0"
-                        u"\U000024C2-\U0001F251"
-                        u"\U0001f926-\U0001f937"
-                        u'\U00010000-\U0010ffff'
-                        u"\u200d"
-                        u"\u2640-\u2642"
-                        u"\u2600-\u2B55"
-                        u"\u23cf"
-                        u"\u23e9"
-                        u"\u231a"
-                        u"\u3030"
-                        u"\ufe0f"
-            "]+", flags=re.UNICODE)
-    
-        #removing any identified emojis from text 
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   u"\U0001f926-\U0001f937"
+                                   u'\U00010000-\U0010ffff'
+                                   u"\u200d"
+                                   u"\u2640-\u2642"
+                                   u"\u2600-\u2B55"
+                                   u"\u23cf"
+                                   u"\u23e9"
+                                   u"\u231a"
+                                   u"\u3030"
+                                   u"\ufe0f"
+                                   "]+", flags=re.UNICODE)
+
+        # removing any identified emojis from text
         words = emoji_pattern.sub(r'', text)
         return words
-    
+
     def __iter__(self):
         for file_path in self.list_news_files:
             with open(file_path, 'r') as f:
-                CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, remove_stopwords]
-                text = json.load(f)['text']
-                
-                #removes emojis from text 
-                words = NewsContent.remove_emoji(text)
+                doc = json.load(f)
+                assert self.feature_type in doc.keys(), "feature not in the document: " + file_path
+                # without stemming
+                CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces,
+                                  strip_numeric, remove_stopwords]
+                feature = doc[self.feature_type]
+                # self.title = json.load(f)['text']
+                words = self.remove_emoji(feature)
                 words = preprocess_string(words, filters=CUSTOM_FILTERS)
-                # print(words)
                 yield words
 
     '''
@@ -68,4 +76,36 @@ class NewsContent(object):
             for f in files:
                 if f.endswith("." + file_ext) and len(dirs) == 0:
                     list_news_files.append(join(root, f))
-        return iter(list_news_files)
+        return list_news_files
+
+    # def get_title(self):
+
+
+def tsne_similar_word_plot(model, word):
+    "Creates and TSNE model and plots it"
+    labels = [word]
+    tokens = []
+
+    for word, _ in model.similar_by_word(word):
+        tokens.append(model[word])
+        labels.append(word)
+
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(16, 16))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
