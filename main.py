173b3999b1a9e32f4ffc04d243997b3c73dc6ca8
GETT521 <44852625+GETT521@users.noreply.github.com>
2019-06-14 15:00:26 -0400

    documentation

--- main.py
+++ main.py
@@ -15,14 +15,23 @@ from word2vecfeature import Word2VecFeatureGenerator
 # sentences iterable
 # title = NewsContent('../FakeNewsNet/code/fakenewsnet_dataset', 'politifact', 'fake', 'title')
 # content = NewsContent('../FakeNewsNet/code/fakenewsnet_dataset', 'politifact', 'fake', 'text')
+
+# call NewsContent class to preprocess/tokenize the news content
 data = NewsContent('../FakeNewsNet/code/fakenewsnet_dataset', 'politifact', 'fake')
+
+# save news content by specified feature by each sentence on a seperate line, tokens seperated by space
 save_as_line_sentence(data.get_features('title'), "title_ls")
 save_as_line_sentence(data.get_features('text'), "body_ls")
 
 save_as_line_sentence(data.get_features(), "news_ls")
+
+# storing all title and body content into one table for future reference
 data.save_reference_table()
 
+# creates word2vec model for news content
 w2v = Word2VecFeatureGenerator(LineSentence("news_ls"))
+
+# finds similarity in body content with pairs
 sim_vec = w2v.get_title_body_cos_sim(data.get_features("pair"))
 
 # tsne_similar_word_plot(model, "trump")
