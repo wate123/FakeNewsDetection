f0ee37e9d36932c402252d44c5c5074b69e267b6
wate123 <junlin0604@gmail.com>
2019-06-19 13:42:30 -0400

    sentiment feature

--- SentimentFeature.py
+++ SentimentFeature.py
@@ -2,7 +2,8 @@ from nltk import sent_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import json
 import statistics
-from utils import unpack_pair_generator
+from utils import unpack_pair_generator, division
+from collections import defaultdict
 
 
 class SentimentFeatureGenerator(object):
@@ -17,9 +18,15 @@ class SentimentFeatureGenerator(object):
         results = [self.sid.polarity_scores(sentence) for sentence in sentences]
         return self.average_polarity_scores(results)
 
-    def average_polarity_scores(self, results):
-        N = float(len(results))
-        # return {k: sum(t[k] for t in results) / N for k in results[0]}
+    def average_polarity_scores(self, scores):
+        """Mean"""
+        avg_scores = defaultdict(list)
+        for score in scores:
+            for key, val in score.items():
+                avg_scores[key].append(val)
+        for label in avg_scores:
+            avg_scores[label] = statistics.mean(avg_scores[label])
+        return dict(avg_scores)
 
     def process_and_save(self):
         print("Generate Sentiment Features")
@@ -28,7 +35,5 @@ class SentimentFeatureGenerator(object):
             self.pair_news = unpack_pair_generator((title, body) for title, body in data.items())
         self.sentiment_feature["title_polarity_score"] = \
             [self.compute_sentiment(sent_tokenize(news["title"])) for news in self.pair_news]
-
-
-        print(self.sentiment_feature)
-
+        self.sentiment_feature["body_polarity_score"] = \
+            [self.compute_sentiment(sent_tokenize(news["body"])) for news in self.pair_news]
